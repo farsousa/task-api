df658b18fd0374635e68ae77cade1a6ee84a925b
AndersonCaj26 <andersoncaj26@gmail.com>
2022-05-06 00:19:06 -0300

    configuração de documentação com swagger

--- src/main/java/com/farsousa/taskapi/TaskApiApplication.java
+++ src/main/java/com/farsousa/taskapi/TaskApiApplication.java
@@ -2,8 +2,10 @@ package com.farsousa.taskapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class TaskApiApplication {
 
 	public static void main(String[] args) {
