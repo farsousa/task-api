672d2e40b6fb095ac1a9ed028cc08371dd3150ba
Francisco Anderson Rodrigues de Sousa <franciscoanderson@sefaz.pi.gov.br>
2022-05-06 08:19:08 -0300

    ajustando os dados de exemplo

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO task (title, description, status, criation_date) VALUES ('Tarefa 1', 'Alguma coisa 1', 'Aberta', '2022-05-05');
-INSERT INTO task (title, description, status, criation_date) VALUES ('Tarefa 2', 'Alguma coisa 2', 'Aberta', '2022-05-05');
-INSERT INTO task (title, description, status, criation_date) VALUES ('Tarefa 3', 'Alguma coisa 3', 'Aberta', '2022-05-05');
-INSERT INTO task (title, description, status, criation_date) VALUES ('Tarefa 4', 'Alguma coisa 4', 'Aberta', '2022-05-05');
-INSERT INTO task (title, description, status, criation_date) VALUES ('Tarefa 5', 'Alguma coisa 5', 'Aberta', '2022-05-05');
+INSERT INTO task (title, description, status, criation_date) VALUES ('Tarefa 1', 'Descrição 1', 'Aberta', '2022-05-05');
+INSERT INTO task (title, description, status, criation_date) VALUES ('Tarefa 2', 'Descrição 2', 'Fazendo', '2022-05-05');
+INSERT INTO task (title, description, status, criation_date) VALUES ('Tarefa 3', 'Descrição 3', 'Aberta', '2022-05-05');
+INSERT INTO task (title, description, status, criation_date) VALUES ('Tarefa 4', 'Descrição 4', 'Fazendo', '2022-05-05');
+INSERT INTO task (title, description, status, criation_date, conclusion_date) VALUES ('Tarefa 5', 'Descrição 5', 'Concluída', '2022-05-02', '2022-05-05');
